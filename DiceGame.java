a3a6fe5e1667af56c5dc9b0ce255125dea492991
Shimanuki Kodai <s1300139@u-aizu.ac.jp>
2023-06-09 18:10:18 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,12 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class DiceGame{
     public static void main(String args[]){
 	Random rnd = new Random();
+	Scanner s = new Scanner(System.in);
 
 	int sum = 0;
 	int dice = rnd.nextInt(5)+1;
 	sum += dice;
+
+	System.out.println("What is your name?");
+	String name = s.nextLine();
+	System.out.println("Hello, "+name+"!");
 
 	System.out.println("Rolling dice...");
 	System.out.println("Die 1: "+dice);
